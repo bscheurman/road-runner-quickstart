f03abc6e1b304a09105d350b7b6843a65445489e
Brad Scheurman <bscheurman@mymobilecoverage.com>
2024-10-03 23:46:23 -0600

    add MeepMeep simulator

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,39 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(90, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setDimensions(16,16)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, -61, Math.toRadians(90)))
+
+                .lineToY(-40)
+                .turn(Math.toRadians(90))
+                .lineToX(-54)
+                .turn(Math.toRadians(90))
+
+                //.setTangent(Math.toRadians(90+70))
+                //.lineToXSplineHeading(-44, Math.toRadians(225))
+                //.setTangent(Math.toRadians(225))
+                //.lineToY(-54)
+                //.turnTo(Math.toRadians(270))
+
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
